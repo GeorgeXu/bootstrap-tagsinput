9076afdce430fcac1a545da4adbeed947c464046
GeorgeXu <xuge123@gmail.com>
2015-05-16 17:23:17 +0800

    Update bootstrap-tagsinput.js

--- dist/bootstrap-tagsinput.js
+++ dist/bootstrap-tagsinput.js
@@ -43,7 +43,9 @@
 
     this.$container = $('<div class="bootstrap-tagsinput"></div>');
     this.$input = $('<input type="text" placeholder="' + this.placeholderText + '"/>').appendTo(this.$container);
-
+    if(this.$element.prop('disabled')){
+      this.$input.prop('disabled',true);
+    }
     this.$element.before(this.$container);
 
     this.build(options);
